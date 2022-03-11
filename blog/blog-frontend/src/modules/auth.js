0a690dd971415785cf535ea58258dc6597542444
olo90632951 <olo90632951@gmail.com>
2022-03-11 12:13:30 +0900

    ..

--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,33 +1,30 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
 import { takeLatest } from 'redux-saga/effects';
-import createRequestSaga, {
-  createRequestActionTypes,
-} from '../lib/createRequestSaga';
+import createRequestSaga from '../lib/createRequestSaga';
 import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
-  'auth/REGISTER',
-);
+const REGISTER = 'auth/REGISTER';
+const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
 
-const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
-  'auth/LOGIN',
-)
-
+const LOGIN = 'auth/LOGIN';
+const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
   CHANGE_FIELD,
   ({ form, key, value }) => ({
     form,
     key,
-    value,    
+    value,
   }),
 );
 
-export const initializeForm = createAction(INITIALIZE_FORM, form=> form);
+export const initializeForm = createAction(INITIALIZE_FORM);
 
 export const register = createAction(REGISTER, ({ username, password }) => ({
   username,
@@ -41,7 +38,7 @@ export const login = createAction(LOGIN, ({ username, password }) => ({
 
 const registerSaga = createRequestSaga(REGISTER, authAPI.register);
 const loginSaga = createRequestSaga(LOGIN, authAPI.login);
-export function* authSaga(){
+export function* authSaga() {
   yield takeLatest(REGISTER, registerSaga);
   yield takeLatest(LOGIN, loginSaga);
 }
@@ -52,9 +49,9 @@ const initialState = {
     password: '',
     passwordConfirm: '',
   },
-  login:{
-    username:'',
-    password:'',
+  login: {
+    username: '',
+    password: '',
   },
   auth: null,
   authError: null,
@@ -62,40 +59,39 @@ const initialState = {
 
 const auth = handleActions(
   {
-    [CHANGE_FIELD]: (state, {payload: {form, key, value} }) => 
-      produce(state, draft => {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
         draft[form][key] = value;
       }),
-    [INITIALIZE_FORM]: (state, {payload: form}) => ({
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
       ...state,
       [form]: initialState[form],
-      authError: null,   //폼 전환 시 회원 인증 에러 초기화      
+      authError: null, //폼 전환 시 회원 인증 에러 초기화
     }),
     //회원가입 성공
-    [REGISTER_SUCCESS]: (state, {payload:auth}) => ({
+    [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
       ...state,
       authError: null,
       auth,
     }),
     //회원가입 실패
-    [REGISTER_FAILURE]: (state, {payload:error}) => ({
+    [REGISTER_FAILURE]: (state, { payload: error }) => ({
       ...state,
       authError: error,
     }),
     //로그인 성공
-    [LOGIN_SUCCESS]: (state, {payload:auth}) => ({
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
       ...state,
       authError: null,
       auth,
     }),
     //로그인 실패
-    [LOGIN_FAILURE]: (state, {payload:error}) => ({
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
       ...state,
       authError: error,
-    }),    
+    }),
   },
   initialState,
 );
 
 export default auth;
-
--- blog/blog-frontend/src/modules/loading.js
+++ blog/blog-frontend/src/modules/loading.js
@@ -3,30 +3,24 @@ import { createAction, handleActions } from 'redux-actions';
 const START_LOADING = 'loading/START_LOADING';
 const FINISH_LOADING = 'loading/FINISH_LOADING';
 
-export const startLoading = createAction(
-    START_LOADING,
-    requestType => requestType,
-);
+export const startLoading = createAction(START_LOADING);
 
-export const finishLoading = createAction(
-    FINISH_LOADING,
-    requestType => requestType,
-);
+export const finishLoading = createAction(FINISH_LOADING);
 
 const initialState = {};
 
 const loading = handleActions(
-    {
-        [START_LOADING]: (state,action) => ({
-            ...state,
-            [action.pay]: true,
-        }),
-        [FINISH_LOADING]: (state,action) => ({
-            ...state,
-            [action.payload]: false,
-        }),
-    },
-    initialState,
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.pay]: true,
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false,
+    }),
+  },
+  initialState,
 );
 
 export default loading;
