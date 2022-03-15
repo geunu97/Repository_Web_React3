4e72e853fd71da264ec5406830984e8d04f0538b
olo90632951 <olo90632951@gmail.com>
2022-03-16 00:22:59 +0900

    ..

--- blog/blog-frontend/src/lib/api/posts.js
+++ blog/blog-frontend/src/lib/api/posts.js
@@ -1,5 +1,18 @@
+import qs from 'qs';
 import client from './client';
 
 //게시물 작성
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
+
+//게시물 읽기
+export const readPost = (id) => client.get(`/api/posts/${id}`);
+
+export const listPosts = ({ page, username, tag }) => {
+  const queryString = qs.stringify({
+    page,
+    username,
+    tag,
+  });
+  return client.get(`/api/posts?${queryString}`);
+};
